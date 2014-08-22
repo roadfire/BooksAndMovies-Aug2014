1663a1c89c76a656f6aefce16f06063383560e87
Josh Brown <josh@roadfiresoftware.com>
2014-08-22 11:25:43 -0400

    Initial Commit

---
+++ BooksAndMovies/RFSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RFSAppDelegate.h
+//  BooksAndMovies
+//
+//  Created by Josh Brown on 8/22/14.
+//  Copyright (c) 2014 Roadfire Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RFSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BooksAndMovies/RFSViewController.h
@@ -0,0 +1,13 @@
+//
+//  RFSViewController.h
+//  BooksAndMovies
+//
+//  Created by Josh Brown on 8/22/14.
+//  Copyright (c) 2014 Roadfire Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RFSViewController : UIViewController
+
+@end
